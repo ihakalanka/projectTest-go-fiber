11da2790e2bc8def919a7116f98974d079bd2bcf
akalankaih <akalankaih.19@uom.lk>
2022-04-24 22:32:55 +0530

    add reset password page

---
+++ frontend/src/components/reset.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import {Link, Navigate} from "react-router-dom";
+import axios from "axios";
+
+const Reset = (props) => {
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [navigate, setNavigate] = useState(false);
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        await axios.post('http://localhost:8080/api/register', {
+            token: props.match.params.token,
+            password,
+            password_confirm: passwordConfirm
+        });
+
+        setNavigate(true);
+    }
+
+    if (navigate) {
+        return <Navigate to="/login"/>;
+    }
+
+    return (
+        <main className="form-signin">
+            <form onSubmit={submit}>
+                <h1 className="h3 mb-3 fw-normal">Please reset your password</h1>
+
+                <div className="form-floating">
+                    <input type="password" className="form-control mb-3" id="floatingPassword" placeholder="Password"
+                           onChange={e => setPassword(e.target.value)}
+                    />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+
+                <div className="form-floating">
+                    <input type="password" className="form-control mb-3" id="floatingPassword" placeholder="Password Confirm"
+                           onChange={e => setPasswordConfirm(e.target.value)}
+                    />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+
+                <div className="mb-2" >
+                    <Link to="/forgot">
+                        Forgot password?
+                    </Link>
+                </div>
+
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+            </form>
+        </main>
+    );
+};
+
+export default Reset;
