375ddcb0e5e9d2d6935bba95f44047452a9ecf28
akalankaih <akalankaih.19@uom.lk>
2022-04-24 22:35:33 +0530

    add frontend routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Login} from "./components/Login";
+import {Register} from "./components/Register";
+import {Home} from "./components/Home";
+import {Nav} from "./components/nav";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Forgot from "./components/forgot";
+import Reset from "./components/reset";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [firstName, setFirstName] = useState('');
+  useEffect(() => {
+    (
+        async () => {
+          await axios.get(`http://localhost:8080/api/user`, {withCredentials: true})
+              .then((res)=>{
+                setFirstName(res.data.firstName);
+              })
+              .catch((err)=> {
+                console.log(err)
+              })
+        }
+    )();
+  },[])
+  return <BrowserRouter>
+    <Nav/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/forgot" element={<Forgot/>}/>
+      <Route path="/reset/:token" element={<Reset/>}/>
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
